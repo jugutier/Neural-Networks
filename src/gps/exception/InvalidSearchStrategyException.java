ebd93da09a2cdf2b7f8a4f9f6a38baaf17321a02
Javier Perez Cuñarro <jperezcu@itba.edu.ar>
2014-04-15 20:21:46 -0300

    moving to folder tpe1

--- src/gps/exception/InvalidSearchStrategyException.java
+++
@@ -1,8 +0,0 @@
-package gps.exception;
-
-/**
- * Created by alexism on 22/03/14.
- */
-public class InvalidSearchStrategyException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-}
--- src/mahjong/Tile.java
+++
@@ -1,8 +0,0 @@
-package mahjong;
-
-/**
- * Created by jugutier on 18/03/14.
- */
-public enum Tile {
-    // ONE=1,TWO = 2,SUMMER
-}
