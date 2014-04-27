ba1d87ce4142acbf3973283ef2f845f7af35b003
Javier Perez Cuñarro <jperezcu@live.com>
2014-04-27 17:51:18 -0300

    fix normalize

--- tpe2/normalize.m
+++ tpe2/normalize.m
@@ -1,7 +1,9 @@
 function normalize(Filename)
 	CSV = dlmread(Filename, ',');
 	for i=1:rows(CSV)
-		CSV(i,:) =  CSV(i,:) / norm(CSV(i,:))
+		if CSV(i,:) != 0
+			CSV(i,:) =  CSV(i,:) / norm(CSV(i,:))
+		endif
 	endfor
 	name = strsplit(Filename,".");
 	finalName = strcat(name{1},"norm.csv");
