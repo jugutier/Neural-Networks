907a7a7bfbd4ae8d8a922dedeb7dfd8a82bb3246
Alexis Medvedeff <alexis.medvedeff@sabf.org.ar>
2014-05-18 18:44:14 -0300

    Genetic algorithm first commit

---
+++ tpe4/genetic.m
@@ -0,0 +1,47 @@
+function [ out ] = genetic(operator, selectionMethod, replacementCriterion, replacementMethod, progenitorsNumber, finalizeCriterion, maxGenerations, individuals, mutationProbability)
+
+# Arquitectura a utilizar: [5 4]
+layers = [5 4];
+
+# Initialize the population with random values
+# Each individual is a matrix of weights (floats)
+population = cell(individuals, 1);
+for i = 1 : individuals
+	population{i} = rand(5, 4); #Matriz de pesos para el individuo i
+endfor
+
+
+#Calculate the fitness for all the individuals in the population
+fitnessAll = evaluateFitness(population);
+
+#while (condicion de corte)
+#	Seleccionar individuos para reproduccion
+#	Recombinar individuos
+#	Mutar individuos
+#	evaluar fitness de los individuos obtenidos
+#	generar nueva poblacion
+
+while (!finalizeCriterion)
+	individualsToReproduce = chooseIndividuals(population);
+	newIndividuals = reproduceIndividuals(individualsToReproduce);
+	newIndividuals = mutateIndividuals(newIndividuals);
+	evaluateFitness(newIndividuals);
+	population = generatePopulation(newIndividuals, evaluateFitness, population);
+endwhile
+
+endfunction
+
+
+# Fitness function. Receives the population matrix and returns a new matrix with 
+# the values of the fitness for each individual
+function out = evaluateFitness(population) 
+	individuals = (size(population))(1)
+	for i = 1 : individuals
+		out(i) =  1 / meanSquareError(population{i}); # How do we calculate the fitness for an individual
+	endfor
+endfunction
+
+# Returns the mean square error for the weights matrix m
+function e = meanSquareError(m)
+	e = 2; #TODO: calculate error
+endfunction
