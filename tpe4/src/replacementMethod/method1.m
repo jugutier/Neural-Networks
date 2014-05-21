9a202ee723d86fd548b2e91f2b838ac6c681f383
Alexis Medvedeff <alexis.medvedeff@sabf.org.ar>
2014-05-21 00:15:57 -0300

    Replacement methods

---
+++ tpe4/src/replacementMethod/method1.m
@@ -0,0 +1,3 @@
+function out = method1(newIndividuals,individualsToReproduce, populationInArrays,replacementCriterion)
+ out = [newIndividuals populationInArrays];
+endfunction
---
+++ tpe4/src/replacementMethod/method2.m
@@ -0,0 +1,4 @@
+function out = method2(newIndividuals,individualsToReproduce, populationInArrays,replacementCriterion)
+ N = [ individualsToReproduce populationInArrays];
+ k = newIndividuals;
+end
