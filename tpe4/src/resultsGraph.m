0c9cd340f7626c74f832d30f517b52a9266e1b4d
Alexis Medvedeff <alexis.medvedeff@sabf.org.ar>
2014-05-25 01:56:40 -0300

    Update graphs

--- tpe4/src/resultsGraph.m
+++ tpe4/src/resultsGraph.m
@@ -1,7 +1,7 @@
  function resultsGraph(mean_fitness_generations, best_fitness_generations, last_index)
    	clf;  hold on;
-	plot(1:last_index, mean_fitness_generations(:,1:last_index), '-1; Fitness promedio;');
-  	plot(1:last_index,best_fitness_generations(:,1:last_index), '-2; Fitness mas alto;');
+	plot(1:last_index, mean_fitness_generations(:,1:last_index), '1');
+  	plot(1:last_index,best_fitness_generations(:,1:last_index), '3');
   	plot(last_index, 0);
   	xlabel("Numero de Generacion");
   	ylabel("Fitness");
