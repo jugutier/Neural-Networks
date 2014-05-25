488fbef9d7d750cb1fe6135a4523245360ac19e8
Alexis Medvedeff <alexis.medvedeff@sabf.org.ar>
2014-05-25 12:35:16 -0300

    Fix anular corssover

--- tpe4/src/crossOver/anularCrossover.m
+++ tpe4/src/crossOver/anularCrossover.m
@@ -1,6 +1,7 @@
 function progenitors = anularCrossover(individual1, individual2)
 	r = floor(rand() * length(individual1));
 	l = floor(rand() * length(individual1) / 2 + 1);
+	
 	i = 1;
 	while(i < length(individual2) && i < r)
 		son1(i) = individual1(i);
@@ -8,21 +9,20 @@ function progenitors = anularCrossover(individual1, individual2)
 		i++;
 	endwhile
 
-	i=0;
 	while(i < length(individual2) && i < (r + l))
-		son1(i) = individual1(i);
-		son2(i) = individual2(i);
+		son1(i) = individual2(i);
+		son2(i) = individual1(i);
 		i++;
 	endwhile
 	
-	if (length(individual1) - (l+r) < 0 ) 
-		i=0;
-		while(i < ((l+r) - length(individual1)))
+	%if (length(individual1) - (l+r) < 0 ) 
+		%while(i < ((l+r) - length(individual1)))
+		while(i <= length(individual1))
 			son2(i) = individual2(i);
 			son1(i) = individual1(i);
 			i++;
 		endwhile
-	endif
+	%endif
 
 	progenitors{1} = son1;
 	progenitors{2} = son2;
